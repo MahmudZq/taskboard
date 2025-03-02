0b60c25b15872fd243c3bc0617e25fb1e3cdc294
Mahmud Rayhan <mahmudrayhan.j@gmail.com>
2025-03-02 14:58:22 +0600

    removed test consoles from index.js

--- js/index.js
+++ js/index.js
@@ -5,13 +5,3 @@ for(let i=0; i<cardTextlist.length; i++){
 }
 
 dateTime();
-
-
-const test = document.getElementById('testForBlog');
-const inHt = test.innerHTML;
-const inTx = test.innerText;
-const txCt = test.textContent;
-console.log(test);
-console.log(inHt);
-console.log(inTx);
-console.log(txCt);
